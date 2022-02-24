11aa87118beeb72aa1807c71577f3cd3e8498bde
rkdwngns <93520535+rkdwngns@users.noreply.github.com>
2022-02-24 16:33:05 +0900

    Add files via upload

---
+++ 이름없음1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+	int main()
+	{
+	int i;
+	int j;
+	printf("값의 범위를 입력해주세요.");
+		scanf("%d", &j);
+	
+	for(i=1;i<=j;i++){
+	if(i/10==3 && (i%10==3 || i%10==6 || i%10==9))
+	printf("짝짝 ");
+	else if(i/10==3 || i%10==3 || i%10==6 || i%10==9)
+	printf("짝 ");
+	else printf("%d ", i);
+	}
+	return 0;
+}
+	
